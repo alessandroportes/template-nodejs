611fb39e6e84a7d16e1c46cdd29ce14e40627446
Alessandro Portes <alessandro.portes@gmail.com>
2020-06-26 07:45:32 -0300

    Added APP, ROUTES, SERVER

---
+++ api/src/routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+const routes = new Router();
+
+routes.get('/', (request, response) => {
+  // rota exemplo
+  response.json({ message: 'Hello World' });
+});
+
+export default routes;
